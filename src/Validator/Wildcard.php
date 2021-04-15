c36c51d44d4a5224ce7d89658c9b0df314d05c9b
Christy Jacob <christyjacob4@gmail.com>
2021-04-16 01:54:08 +0530

    feat: added wildcard validator

---
+++ src/Validator/Wildcard.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Utopia PHP Framework
+ *
+ * @package Framework
+ * @subpackage Validator
+ *
+ * @link https://github.com/utopia-php/framework
+ * @author Appwrite Team <team@appwrite.io>
+ * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+namespace Utopia\Validator;
+
+use Utopia\Validator;
+
+/**
+ * Wildcard
+ *
+ * Does not perform any validation. Always returns valid
+ *
+ * @package Utopia\Validator
+ */
+class Wildcard extends Validator
+{
+    /**
+     * Get Description
+     *
+     * Returns validator description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Every input is valid';
+    }
+
+    /**
+     * Is valid
+     *
+     * Validation will pass when $value is numeric.
+     *
+     * @param  mixed $value
+     * @return bool
+     */
+    public function isValid($value)
+    {
+        return true;
+    }
+
+    /**
+     * Is array
+     *
+     * Function will return true if object is array.
+     *
+     * @return bool
+     */
+    public function isArray(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get Type
+     *
+     * Returns validator type.
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return self::TYPE_STRING;
+    }
+}
