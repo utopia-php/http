f5b8933edf5f5dc26e5a939e01219575139eef27
Matej Bačo <matejbaco2000@gmail.com>
2022-02-03 09:36:58 +0000

    Added hostname validator

---
+++ src/Validator/Hostname.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Utopia PHP Framework
+ *
+ * @package Framework
+ * @subpackage Validator
+ *
+ * @link https://github.com/utopia-php/framework
+ * @author Appwrite Team <team@appwrite.io>
+ * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+namespace Utopia\Validator;
+
+use Utopia\Validator;
+
+class Hostname extends Validator
+{
+    /**
+     * @var string[]
+     */
+    protected array $whitelist = [];
+
+    /**
+     * Constructor
+     *
+     * Sets allowed hostname patterns
+     *
+     * @param string[] $whitelist
+     */
+    public function __construct(array $whitelist = [])
+    {
+        $this->whitelist = $whitelist;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'Value must be a hostname without path, port and protocol.';
+    }
+
+    /**
+     * Is array
+     *
+     * Function will return true if object is array.
+     *
+     * @return bool
+     */
+    public function isArray(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get Type
+     *
+     * Returns validator type.
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return self::TYPE_STRING;
+    }
+
+    /**
+     * @param mixed $value
+     * @return bool
+     */
+    public function isValid(mixed $value): bool
+    {
+        if (!\is_string($value) || empty($value)) {
+            return false;
+        }
+
+        // This tests: 'http://', 'https://', and 'myapp.com/route'
+        if (\str_contains($value, '/')) {
+            return false;
+        }
+
+        // This tests for: 'myapp.com:3000'
+        if (\str_contains($value, ':')) {
+            return false;
+        }
+
+        if(isset($this->whitelist) && !empty($this->whitelist)) {
+            // Loop through all allowed hostnames until match is found
+            foreach ($this->whitelist as $allowedHostname) {
+                // If exact math; allow
+                if($value === $allowedHostname) {
+                    return true;
+                }
+
+                // If wildcard symbol used
+                if(\str_contains($allowedHostname, '*')) {
+                    // Split hostnames into sections (subdomains)
+                    $allowedSections = \explode('.', $allowedHostname);
+                    $valueSections = \explode('.', $value);
+
+                    // Only if amount of sections matches
+                    if(\count($allowedSections) === \count($valueSections)) {
+                        $matchesAmount = 0;
+
+                        // Loop through all sections
+                        for ($sectionIndex = 0; $sectionIndex < \count($allowedSections); $sectionIndex++) {
+                            $allowedSection = $allowedSections[$sectionIndex];
+
+                            // If section matches, or wildcard symbol is used, increment match count
+                            if($allowedSection === '*' || $allowedSection === $valueSections[$sectionIndex]) {
+                                $matchesAmount++;
+                            } else {
+                                // If one fails, the whole check always fails; we can skip iterations
+                                break;
+                            }
+                        }
+
+                        // If every section matched; allow
+                        if($matchesAmount === \count($allowedSections)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            // If finished loop above without result, match is not found
+            return false;
+        }
+
+        return true;
+    }
+}
