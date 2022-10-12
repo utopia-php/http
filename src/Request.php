06e5d417630ed593a41f3baa694e920d21e51044
Aadeesh11 <asoswal@gmail.com>
2022-10-12 17:46:42 +0530

    Removed authors from all files.

--- src/Request.php
+++ src/Request.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * Utopia PHP Framework
- *
- * @package Framework
- * @subpackage Core
- *
- * @link https://github.com/utopia-php/framework
- * @author Appwrite Team <team@appwrite.io>
- * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
- */
-
 namespace Utopia;
 
 class Request
