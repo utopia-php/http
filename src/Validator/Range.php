52cb316e5c44bf1b7fb1f9af3fa2d8f2392bc7d5
Christy Jacob <christyjacob4@gmail.com>
2023-04-06 14:54:34 +0000

    feat: review comments

--- src/Validator/Range.php
+++ src/Validator/Range.php
@@ -131,11 +131,10 @@ class Range extends Numeric
                 }
                 break;
             case self::TYPE_FLOAT:
-                $value = $value + 0;
-                /** @phpstan-ignore-next-line */
-                if (! is_float($value) && ! is_int($value)) {
+                if (! is_numeric($value) ) {
                     return false;
                 }
+                $value = $value + 0.0;
                 break;
             default:
                 return false;
