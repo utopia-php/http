e84ceaeda253c5b942972450dec73b91405c1cf7
Eldad Fux <eldad.fux@gmail.com>
2024-04-18 23:04:25 +0200

    Fixed inheritance

--- src/Http/Validator.php
+++ src/Http/Validator.php
@@ -6,4 +6,17 @@ use Utopia\Servers\Validator as ServersValidator;
 
 abstract class Validator extends ServersValidator
 {
+    public const TYPE_BOOLEAN = 'boolean';
+
+    public const TYPE_INTEGER = 'integer';
+
+    public const TYPE_FLOAT = 'double'; /* gettype() returns 'double' for historical reasons */
+
+    public const TYPE_STRING = 'string';
+
+    public const TYPE_ARRAY = 'array';
+
+    public const TYPE_OBJECT = 'object';
+
+    public const TYPE_MIXED = 'mixed';
 }
