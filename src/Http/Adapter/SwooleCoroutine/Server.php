6d5c75a042e7a01c5896237e28f5411fd80c9d88
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-07-03 14:33:22 -0400

    feat: adding Coroutine server

---
+++ src/Http/Adapter/SwooleCoroutine/Server.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Utopia\Http\Adapter\SwooleCoroutine;
+
+use Swoole\Coroutine;
+use Utopia\Http\Adapter;
+use Swoole\Coroutine\Http\Server as SwooleServer;
+use Swoole\Http\Request as SwooleRequest;
+use Swoole\Http\Response as SwooleResponse;
+use Utopia\Http\Http;
+
+use function Swoole\Coroutine\run;
+
+class Server extends Adapter
+{
+    protected SwooleServer $server;
+
+    public function __construct(string $host, string $port = null, array $settings = [])
+    {
+        $this->server = new SwooleServer($host, $port);
+        $this->server->set(\array_merge($settings, [
+            'enable_coroutine' => true
+        ]));
+    }
+
+    public function onRequest(callable $callback)
+    {
+        $this->server->handle('/', function (SwooleRequest $request, SwooleResponse $response) use ($callback) {
+            $context = \strval(Coroutine::getCid());
+
+            Http::setResource('swooleRequest', fn () => $request, [], $context);
+            Http::setResource('swooleResponse', fn () => $response, [], $context);
+
+            call_user_func($callback, new Request($request), new Response($response), $context);
+        });
+    }
+
+    public function onStart(callable $callback)
+    {
+        call_user_func($callback, $this);
+    }
+
+    public function start()
+    {
+        if(Coroutine::getCid() === -1) {
+            run(fn () => $this->server->start());
+        } else {
+            $this->server->start();
+        }
+    }
+}
