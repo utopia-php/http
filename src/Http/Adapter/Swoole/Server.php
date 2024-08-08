ee50ec949d51f8e12b80cf05a39973140ac05dd2
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-08-08 10:40:27 -0400

    chore: cleanup

--- src/Http/Adapter/Swoole/Server.php
+++ src/Http/Adapter/Swoole/Server.php
@@ -12,24 +12,10 @@ class Server extends Adapter
 
     public function __construct(string $host, string $port = null, array $settings = [])
     {
-        $workerNumber = swoole_cpu_num() * 6;
         $this->server = new SwooleServer($host, $port);
         $this->server->set(\array_merge($settings, [
             'open_http2_protocol' => true,
-            // 'http_compression' => true,
-            // 'http_compression_level' => 6,
-
-            // Server
-            // 'log_level' => 0,
             'dispatch_mode' => 2,
-            // 'worker_num' => $workerNumber,
-            // 'reactor_num' => swoole_cpu_num() * 2,
-            // 'task_worker_num' => $workerNumber,
-            // 'open_cpu_affinity' => true,
-
-            // Coroutine
-            // 'enable_coroutine' => true,
-            // 'max_coroutine' => 1000,
         ]));
     }
 
