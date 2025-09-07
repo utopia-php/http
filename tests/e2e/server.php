e745e7b2dbb78d69a409c8695ec64ed6c0bbd021
Matej Bačo <matejbaco2000@gmail.com>
2025-09-07 10:55:02 +0200

    Add array-like headers; sync partially with 0.34.x

--- tests/e2e/server.php
+++ tests/e2e/server.php
@@ -26,6 +26,22 @@ App::get('/value/:value')
         $response->send($value);
     });
 
+App::get('/cookies')
+    ->inject('request')
+    ->inject('response')
+    ->action(function (Request $request, Response $response) {
+        $response->send($request->getHeaders()['cookie'] ?? '');
+    });
+
+App::get('/set-cookie')
+    ->inject('request')
+    ->inject('response')
+    ->action(function (Request $request, Response $response) {
+        $response->addHeader('Set-Cookie', 'key1=value1');
+        $response->addHeader('Set-Cookie', 'key2=value2');
+        $response->send('OK');
+    });
+
 App::get('/chunked')
     ->inject('response')
     ->action(function (Response $response) {
