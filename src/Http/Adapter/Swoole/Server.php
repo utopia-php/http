6487faba67ce07fe833c07552e299f81c61311ea
Damodar Lohani <dlohani48@gmail.com>
2025-10-13 00:48:29 +0000

    keep original server

--- src/Http/Adapter/Swoole/Server.php
+++ src/Http/Adapter/Swoole/Server.php
@@ -2,13 +2,8 @@
 
 namespace Utopia\Http\Adapter\Swoole;
 
-use Swoole\Coroutine;
 use Utopia\Http\Adapter;
-use Swoole\Coroutine\Http\Server as SwooleServer;
-use Swoole\Http\Request as SwooleRequest;
-use Swoole\Http\Response as SwooleResponse;
-
-use function Swoole\Coroutine\run;
+use Swoole\Http\Server as SwooleServer;
 
 class Server extends Adapter
 {
@@ -17,32 +12,32 @@ class Server extends Adapter
     public function __construct(string $host, ?string $port = null, array $settings = [])
     {
         $this->server = new SwooleServer($host, $port);
-        $this->server->set(array_merge([
+        $this->server->set(\array_merge($settings, [
             'open_http2_protocol' => true,
             'dispatch_mode' => 2,
-            'enable_coroutine' => true,
-            'http_parse_cookie' => false,
-        ], $settings));
+        ]));
     }
 
     public function onRequest(callable $callback)
     {
-        $this->server->handle('/', function (SwooleRequest $request, SwooleResponse $response) use ($callback) {
-            call_user_func($callback, $request, $response);
+        $this->server->on('request', function ($request, $response) use ($callback) {
+            go(function () use ($request, $response, $callback) {
+                call_user_func($callback, new Request($request), new Response($response));
+            });
         });
     }
 
     public function onStart(callable $callback)
     {
-        call_user_func($callback, $this);
+        $this->server->on('start', function () use ($callback) {
+            go(function () use ($callback) {
+                call_user_func($callback);
+            });
+        });
     }
 
     public function start()
     {
-        if (Coroutine::getCid() === -1) {
-            run(fn () => $this->server->start());
-        } else {
-            $this->server->start();
-        }
+        return $this->server->start();
     }
 }
